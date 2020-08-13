4168e5c38e47ca594602694b42399feea1a43411
pranshu shah <pranshu.shah23@gmail.com>
2020-08-13 11:17:49 +0530

    Updates

---
+++ precache-manifest.4a68f7114fe7f9b420afe4887164ba97.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c348cb895e83b6b351b390764749b0ac",
+    "url": "/dbonline/index.html"
+  },
+  {
+    "revision": "79f0bbdf3809090f7a85",
+    "url": "/dbonline/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "e9bd44d7f710044b963a",
+    "url": "/dbonline/static/css/main.8464b6a5.chunk.css"
+  },
+  {
+    "revision": "79f0bbdf3809090f7a85",
+    "url": "/dbonline/static/js/2.84a99233.chunk.js"
+  },
+  {
+    "revision": "6519c79c5d353aaf778964e0125e54e6",
+    "url": "/dbonline/static/js/2.84a99233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9bd44d7f710044b963a",
+    "url": "/dbonline/static/js/main.7c8ba3a0.chunk.js"
+  },
+  {
+    "revision": "43f0cd9946d4bc3bbe45",
+    "url": "/dbonline/static/js/runtime-main.bc5c401d.js"
+  },
+  {
+    "revision": "5a1545d8dd8b05dbddcbba14b374c34f",
+    "url": "/dbonline/static/media/alert.5a1545d8.svg"
+  },
+  {
+    "revision": "82d087e138d4b09b62d130a961361a1e",
+    "url": "/dbonline/static/media/cc.82d087e1.svg"
+  },
+  {
+    "revision": "90a4f102a27ff518d302ad32101e8686",
+    "url": "/dbonline/static/media/close.90a4f102.svg"
+  },
+  {
+    "revision": "7838f02af6cf2dffe03e04c6eede03f3",
+    "url": "/dbonline/static/media/down.7838f02a.svg"
+  },
+  {
+    "revision": "4460c32ebcc4f3c12a3143d4e093e768",
+    "url": "/dbonline/static/media/f.4460c32e.svg"
+  },
+  {
+    "revision": "88d7a1185120267e4fcb44f0b2aa2fa6",
+    "url": "/dbonline/static/media/grid (1).88d7a118.svg"
+  },
+  {
+    "revision": "a73dfccb91c3f7c5d77830324586e656",
+    "url": "/dbonline/static/media/p.a73dfccb.svg"
+  }
+]);
